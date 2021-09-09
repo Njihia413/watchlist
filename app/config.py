38b1d4b418c701187205a9d70b2b7868a6c5fd74
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-09 10:55:03 +0300

    Add configuration classes

--- app/config.py
+++ app/config.py
@@ -1,1 +1,27 @@
 #Where we will store our app configurations
+from re import DEBUG
+
+
+class Config:
+    '''
+    Parent configuration class with general configuration settings
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: Parent configuration class with general configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: Parent confuguration class with general configuration settings
+    '''
+    DEBUG = True
