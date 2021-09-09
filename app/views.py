cc02ede3acef2684fb87b3a3ed05cd243e8a3e17
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-09 15:49:46 +0300

    Make few changes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 #Where we will create all our view functions
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie, search_movie
 
@@ -15,6 +15,10 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The Best Movie Review Website Online'
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name = search_movie))
     return render_template('index.html',title = title,popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:id>')
