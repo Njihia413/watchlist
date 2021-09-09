427747fa330ddc3f0602c5c320524f21cc74bd3e
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-09 10:41:56 +0300

    Make few changes

--- app/views.py
+++ app/views.py
@@ -3,9 +3,10 @@ from flask import render_template
 from app import app
 
 #Views
-app.route('/')
+@app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello World'
+    return render_template('index.html',message = message)
