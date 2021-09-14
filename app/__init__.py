c152bbd20b1cff9bd00163224f4c37261c60f59c
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-14 11:52:11 +0300

    Initialize SQLAlchemy extension

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+
+#Initialize SQLAlchemy Extension
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -15,6 +19,7 @@ def create_app(config_name):
 
     #Initializing Flask Extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     #Registering the blueprint
     from .main import main as main_blueprint
